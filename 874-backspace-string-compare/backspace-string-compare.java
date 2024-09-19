820c3d0f8dcbc9e78c930d2e28f1534a8ae0f912
Kiruba-Prakasan <124655692+Kiruba-Prakasan@users.noreply.github.com>
2024-09-19 10:09:26 +0530

    Time: 1 ms (78.95%) | Memory: 42 MB (9.15%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.java
+++ 874-backspace-string-compare/backspace-string-compare.java
@@ -4,20 +4,20 @@ class Solution {
         String s2 = Compare(t);
         return s1.equals(s2);
     }
-    private static String Compare(String s){
+
+    private static String Compare(String s) {
         Stack<Character> st = new Stack<>();
-        for(char ch:s.toCharArray()){
-            if(ch!='#'){
+        for (char ch : s.toCharArray()) {
+            if (ch != '#') {
                 st.push(ch);
-            }
-            else{
-                if(!st.isEmpty()){
-                st.pop();
+            } else {
+                if (!st.isEmpty()) {
+                    st.pop();
                 }
             }
         }
         StringBuilder str = new StringBuilder();
-        while(!st.isEmpty()){
+        while (!st.isEmpty()) {
             str.append(st.pop());
         }
         return str.toString();

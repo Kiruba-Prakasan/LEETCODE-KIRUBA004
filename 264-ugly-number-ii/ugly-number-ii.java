f55eb557f4eac613a0eacd24e041082bfba34b03
Kiruba-Prakasan <124655692+Kiruba-Prakasan@users.noreply.github.com>
2024-09-23 11:34:56 +0530

    Time: 2 ms (96.55%) | Memory: 41.9 MB (62.33%) - LeetSync

--- 264-ugly-number-ii/ugly-number-ii.java
+++ 264-ugly-number-ii/ugly-number-ii.java
@@ -1,28 +1,19 @@
-import java.util.HashSet;
-import java.util.PriorityQueue;
-
 public class Solution {
     public int nthUglyNumber(int n) {
-        PriorityQueue<Long> heap = new PriorityQueue<>();
-        HashSet<Long> seen = new HashSet<>();
-        heap.add(1L);
-        seen.add(1L);
-        long ugly = 1L;
-        for (int i = 0; i < n; i++) {
-            ugly = heap.poll();
-            long nextUgly2 = ugly * 2;
-            long nextUgly3 = ugly * 3;
-            long nextUgly5 = ugly * 5;
-            if (seen.add(nextUgly2)) {
-                heap.add(nextUgly2);
-            }
-            if (seen.add(nextUgly3)) {
-                heap.add(nextUgly3);
-            }
-            if (seen.add(nextUgly5)) {
-                heap.add(nextUgly5);
-            }
+        int[] ugly = new int[n];
+        ugly[0] = 1;
+        int index2 = 0, index3 = 0, index5 = 0;
+        int factor2 = 2, factor3 = 3, factor5 = 5;
+        for(int i=1;i<n;i++){
+            int min = Math.min(Math.min(factor2,factor3),factor5);
+            ugly[i] = min;
+            if(factor2 == min)
+                factor2 = 2*ugly[++index2];
+            if(factor3 == min)
+                factor3 = 3*ugly[++index3];
+            if(factor5 == min)
+                factor5 = 5*ugly[++index5];
         }
-        return (int) ugly;
+        return ugly[n-1];
     }
 }

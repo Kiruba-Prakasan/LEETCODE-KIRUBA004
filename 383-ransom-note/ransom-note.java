ffd7079ac1a20f5c9cdcec5e7911548b418c5f1f
Kiruba-Prakasan <124655692+Kiruba-Prakasan@users.noreply.github.com>
2024-09-25 15:43:23 +0530

    Time: 2 ms (81.22%) | Memory: 44.9 MB (57.82%) - LeetSync

--- 383-ransom-note/ransom-note.java
+++ 383-ransom-note/ransom-note.java
@@ -1,20 +1,20 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
         // Brute Force Approach
+          int[] letters = new int[26];
+          for(char ch : magazine.toCharArray()){
+          letters[ch-'a']++;
+          }
+          for(char ch : ransomNote.toCharArray()){
+          letters[ch-'a']--;
+          if(letters[ch-'a']==-1)
+          {
+          return false;
+          }
+          }
+          return true;
+        //Using HashMap
         /*
-         * int[] letters = new int[26];
-         * for(char ch : magazine.toCharArray()){
-         * letters[ch-'a']++;
-         * }
-         * for(char ch : ransomNote.toCharArray()){
-         * letters[ch-'a']--;
-         * if(letters[ch-'a']==-1)
-         * {
-         * return false;
-         * }
-         * }
-         * return true;
-         */
         HashMap<Character, Integer> map = new HashMap<>();
         for (char ch : magazine.toCharArray()) {
             map.put(ch, map.getOrDefault(ch, 0) + 1);
@@ -26,5 +26,6 @@ class Solution {
             map.put(ch,map.get(ch)-1);
         }
         return true;
+        */
     }
 }

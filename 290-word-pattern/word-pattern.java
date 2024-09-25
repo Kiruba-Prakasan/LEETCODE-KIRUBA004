1bb6108dc6aa20bb40b83c8ea3ceeceb0eb92f12
Kiruba-Prakasan <124655692+Kiruba-Prakasan@users.noreply.github.com>
2024-09-25 15:17:13 +0530

    Time: 2 ms (13.12%) | Memory: 41.4 MB (32.37%) - LeetSync

---
+++ 290-word-pattern/word-pattern.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean wordPattern(String p, String s) {
+        String[] words = s.split("\\s+");
+        HashMap<Character,String> pmap = new HashMap<>();
+        HashMap<String,Character> smap = new HashMap<>();
+        //System.out.println(Arrays.toString(words));
+        if(words.length!=p.length()){
+            return false;
+        }
+        for(int i=0;i<words.length;i++){
+            char ch = p.charAt(i);
+            String w = words[i];
+            if(pmap.containsKey(ch)&&!pmap.get(ch).equals(w)){
+                return false;
+            }
+            if(smap.containsKey(w)&&!smap.get(w).equals(ch)){
+                return false;
+            }
+            pmap.put(ch,w);
+            smap.put(w,ch);
+        }
+        //System.out.println(pmap);
+        return true;
+    }
+}

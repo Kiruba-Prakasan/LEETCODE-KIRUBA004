e206c7073e6aeb4493d18f76bf815f65865f20e4
Kiruba-Prakasan <124655692+Kiruba-Prakasan@users.noreply.github.com>
2024-09-25 11:17:21 +0530

    Time: 13 ms (6.82%) | Memory: 44.9 MB (5.03%) - LeetSync

--- 179-largest-number/largest-number.java
+++ 179-largest-number/largest-number.java
@@ -2,16 +2,41 @@ class Solution {
     public String largestNumber(int[] nums) {
         String[] arr = new String[nums.length];
         StringBuilder lnum = new StringBuilder();
-        for(int i=0;i<nums.length;i++){
+        for (int i = 0; i < nums.length; i++) {
             arr[i] = String.valueOf(nums[i]);
         }
-        Arrays.sort(arr,(a,b)->(b+a).compareTo(a+b));
-        if(arr[0].equals("0")){
+        // Comaparator
+        /* Arrays.sort(arr,(a,b)->(b+a).compareTo(a+b)); */
+        // Using BubbleSort
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (swap(arr[i], arr[j])) {
+                    String temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        if (arr[0].equals("0")) {
             return "0";
         }
-        for(int i=0;i<arr.length;i++){
+        for (int i = 0; i < arr.length; i++) {
             lnum.append(arr[i]);
         }
         return lnum.toString();
+    }
+
+    private static boolean swap(String a, String b) {
+        String ab = a + b;
+        String ba = b + a;
+        for(int i=0;i<ab.length();i++){
+            if (ab.charAt(i) > ba.charAt(i)) {
+                return false;
+            }
+            if (ab.charAt(i) < ba.charAt(i)) {
+                return true;
+            }
+        }
+        return false;
     }
 }

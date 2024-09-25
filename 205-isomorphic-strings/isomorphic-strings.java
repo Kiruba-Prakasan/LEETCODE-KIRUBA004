e99414dc24bf4aa3ba53dcd6d69b087b00637fbf
Kiruba-Prakasan <124655692+Kiruba-Prakasan@users.noreply.github.com>
2024-09-25 11:53:22 +0530

    Time: 23 ms (11.23%) | Memory: 42.2 MB (73.00%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -6,20 +6,11 @@ class Solution {
         for (int i = 0; i < s.length(); i++) {
             char ch1 = s.charAt(i);
             char ch2 = t.charAt(i);
-            if (smap.containsKey(ch1)) {
-                if (smap.get(ch1) != ch2) {
-                    return false;
-                }
-            } else {
-                smap.put(ch1, ch2);
+            if(smap.containsKey(ch1)&&smap.get(ch1)!=ch2||tmap.containsKey(ch2)&&tmap.get(ch2)!=ch1){
+                return false;
             }
-            if (tmap.containsKey(ch2)) {
-                if (tmap.get(ch2) != ch1) {
-                    return false;
-                }
-            } else {
-                tmap.put(ch2, ch1);
-            }
+            smap.put(ch1,ch2);
+            tmap.put(ch2,ch1);
         }
         return true;
     }

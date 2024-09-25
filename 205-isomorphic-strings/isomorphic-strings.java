d9a432db2d57d2fca6bfe8cbb4abb395b85670f1
Kiruba-Prakasan <124655692+Kiruba-Prakasan@users.noreply.github.com>
2024-09-25 11:36:10 +0530

    Time: 24 ms (9.81%) | Memory: 42.4 MB (38.76%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -1,5 +1,7 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
+        //BruteForce Approach
+        /*
         int[] sf = new int[200];
         int[] tf = new int[200];
         if(s.length()!=t.length()){
@@ -11,6 +13,24 @@ class Solution {
             }
             sf[s.charAt(i)]=i+1;
             tf[t.charAt(i)]=i+1;
+        }
+        return true;
+        */
+        //Using HashMap
+        HashMap<Character,Character> smap = new HashMap<>();
+        HashMap<Character,Character> tmap = new HashMap<>();
+
+        for(int i=0;i<s.length();i++){
+            char ch1 = s.charAt(i);
+            char ch2 = t.charAt(i);
+            if(smap.containsKey(ch1)&&smap.get(ch1)!=ch2){
+                return false;
+            }
+            if(tmap.containsKey(ch2)&&tmap.get(ch2)!=ch1){
+                return false;
+            }
+            smap.put(ch1,ch2);
+            tmap.put(ch2,ch1);
         }
         return true;
     }

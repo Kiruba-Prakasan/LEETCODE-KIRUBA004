afc1fdd0e2b68b3510c3d7b7b92032117c92185a
Kiruba-Prakasan <124655692+Kiruba-Prakasan@users.noreply.github.com>
2024-09-26 14:24:27 +0530

    Time: 5 ms (65.96%) | Memory: 42.1 MB (15.76%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.java
+++ 409-longest-palindrome/longest-palindrome.java
@@ -1,20 +1,18 @@
+// 
 class Solution {
     public int longestPalindrome(String s) {
-        int[] arr = new int[256];
-        int c=0;
+        HashSet<Character> set = new HashSet<>();
         int len = 0;
-        for(char ch : s.toCharArray()){
-            arr[ch]++;
-        }
-        for(int f : arr){
-            if(f%2==0){
-                len+=f;
-            }
-            else{
-                len+=f-1;
-                c=1;
+        
+        for (char ch : s.toCharArray()) {
+            if (set.contains(ch)) {
+                len += 2;
+                set.remove(ch);
+            } else {
+                set.add(ch);
             }
         }
-        return (c==0)?len:len+1;
+        
+        return set.isEmpty() ? len : len + 1;
     }
 }

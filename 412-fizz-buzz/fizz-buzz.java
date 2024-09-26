48cc0d387c71fe6bbe10cc3b640986ce29d6d8b3
Kiruba-Prakasan <124655692+Kiruba-Prakasan@users.noreply.github.com>
2024-09-26 15:14:23 +0530

    Time: 1 ms (99.49%) | Memory: 44.8 MB (90.40%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.java
+++ 412-fizz-buzz/fizz-buzz.java
@@ -1,11 +1,16 @@
 class Solution {
     public List<String> fizzBuzz(int n) {
-        List<String> answer = new ArrayList<>();
+        List<String> answer = new ArrayList<String>();
         for (int i = 1; i <= n; i++) {
-            String result = "";
-            if (i % 3 == 0) result += "Fizz";
-            if (i % 5 == 0) result += "Buzz";
-            answer.add(result.isEmpty() ? String.valueOf(i) : result);
+            if (i % 3 == 0 && i % 5 == 0) {
+                answer.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                answer.add("Fizz");
+            } else if (i % 5 == 0) {
+                answer.add("Buzz");
+            } else {
+                answer.add(Integer.toString(i));
+            }
         }
         return answer;
     }

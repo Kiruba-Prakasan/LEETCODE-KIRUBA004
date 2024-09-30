76bca42bb81d90af44923fb8dad5d8af90992340
Kiruba-Prakasan <124655692+Kiruba-Prakasan@users.noreply.github.com>
2024-09-30 17:25:55 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (48.09%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,33 +1,22 @@
 class Solution {
     public void sortColors(int[] nums) {
-        /*
-         * //PriorityQueue Approach
-         * 
-         * PriorityQueue<Integer> queue = new PriorityQueue<>();
-         * for(int num : nums){
-         * queue.add(num);
-         * }
-         * for(int i=0;i<nums.length;i++){
-         * nums[i] = queue.poll();
-         * }
-         */
-
-        int low = 0, mid = 0, high = nums.length - 1;
-        while (mid <= high) {
-            if (nums[mid] == 0) {
-                swap(nums, low, mid);
-                low++;
-                mid++;
-            } else if (nums[mid] == 1) {
-                mid++;
-            } else {
-                swap(nums, mid, high);
-                high--;
+        int l=0,m=0,h=nums.length-1;
+        while(m<=h){
+            if(nums[m]==0){
+                swap(nums,l,m);
+                l++;
+                m++;
+            }
+            else if(nums[m]==1){
+                m++;
+            }
+            else{
+                swap(nums,m,h);
+                h--;
             }
         }
     }
-
-    private static void swap(int[] nums, int i, int j) {
+    private static void swap(int[] nums,int i,int j){
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;

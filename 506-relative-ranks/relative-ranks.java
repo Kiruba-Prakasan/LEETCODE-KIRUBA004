638ccf0acad87a8489c4fb1eef973aaff1a74d88
Kiruba-Prakasan <124655692+Kiruba-Prakasan@users.noreply.github.com>
2024-10-20 12:11:31 +0530

    Time: 8 ms (75.14%) | Memory: 45.5 MB (69.73%) - LeetSync

--- 506-relative-ranks/relative-ranks.java
+++ 506-relative-ranks/relative-ranks.java
@@ -1,5 +1,7 @@
 class Solution {
     public String[] findRelativeRanks(int[] s) {
+        /* Brute Force Approach 
+           Bubble sort O(n^2)
         int n=s.length;
         int[][] sp = new int[n][2];
         for(int i=0;i<n;i++){
@@ -29,6 +31,20 @@ class Solution {
             else{
                 ranks[sp[i][0]]=String.valueOf(i+1);
             }
+        }
+        return ranks;
+        */
+        //Optimised Approach
+        int n = s.length;
+        int[][] sp = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            sp[i][0] = i;
+            sp[i][1] = s[i];
+        }
+        Arrays.sort(sp, (a, b) -> b[1] - a[1]);
+        String[] ranks = new String[n];
+        for (int i = 0; i < n; i++) {
+            ranks[sp[i][0]] = i < 3 ? (i == 0 ? "Gold Medal" : (i == 1 ? "Silver Medal" : "Bronze Medal")) : String.valueOf(i + 1);
         }
         return ranks;
     }

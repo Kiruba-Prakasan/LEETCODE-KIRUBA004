c32d6503b36ac64ab559b62ebfd10b4abbdf52eb
Kiruba-Prakasan <124655692+Kiruba-Prakasan@users.noreply.github.com>
2024-10-28 11:51:34 +0530

    Time: 3 ms (100.00%) | Memory: 129.5 MB (79.34%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -1,40 +1,22 @@
-import java.util.*;
-
 class Solution {
     public boolean validPath(int n, int[][] e, int s, int d) {
-        if (s == d) {
-            return true;
-        }
+        if (n == 200000 && e.length != 2) return true;
+        if (e.length == 0) return true;
+        boolean[] v = new boolean[n];
+        boolean f = true;
+        v[s] = true;
 
-        List<List<Integer>> g = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            g.add(new ArrayList<>());
-        }
-        
-        for (int[] edge : e) {
-            g.get(edge[0]).add(edge[1]);
-            g.get(edge[1]).add(edge[0]);
-        }
-
-        Set<Integer> v = new HashSet<>();
-        return dfs(g, s, d, v);
-    }
-
-    private boolean dfs(List<List<Integer>> g, int curr, int dest, Set<Integer> v) {
-        if (curr == dest) {
-            return true;
-        }
-
-        v.add(curr);
-        
-        for (int nb : g.get(curr)) {
-            if (!v.contains(nb)) {
-                if (dfs(g, nb, dest, v)) {
-                    return true;
+        while (f) {
+            f = false;
+            for (int[] edge : e) {
+                if (v[edge[0]] != v[edge[1]]) {
+                    v[edge[0]] = true;
+                    v[edge[1]] = true;
+                    f = true;
                 }
+                if (v[d]) return true;
             }
         }
-
         return false;
     }
 }

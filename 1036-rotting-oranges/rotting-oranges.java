a8309908c2e655e4a016adc4d4d6bde23d03c8f8
Kiruba-Prakasan <124655692+Kiruba-Prakasan@users.noreply.github.com>
2024-11-04 16:13:27 +0530

    Time: 2 ms (71.52%) | Memory: 42.3 MB (50.58%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,51 +1,49 @@
-class Pair{
-    int row;
-    int col;
-    int level;
-    Pair(int _row,int _col,int _level){
-        this.row=_row;
-        this.col = _col;
-        this.level = _level;
-    }
-}
-class Solution{
-    public int orangesRotting(int [][] grid){
-       Queue<Pair> q = new LinkedList<>();
-       int m = grid.length;
-       int n = grid[0].length;
-       int fr =0;
-       int [][] vis = new int[m][n];
-       for(int i=0;i<m;i++){
-           for(int j=0;j<n;j++){
-               if(grid[i][j]==2){
-                   q.offer(new Pair(i,j,0));
-                   vis[i][j]=1;
-               }
-               if(grid[i][j]==1) fr++;
-           }
-       }
-       int max_level = 0;
-       int [] drow ={0,1,0,-1};
-       int [] dcol ={1,0,-1,0};
-       while(!q.isEmpty()){
-           int r = q.peek().row;
-           int c = q.peek().col;
-           int curr_level = q.peek().level;
-           q.poll();
-           max_level = Math.max(curr_level,max_level);
-           for(int i=0;i<4;i++){
-               int nrow= r+drow[i];
-               int ncol = c + dcol[i];
-               if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && vis[nrow][ncol]==0 && grid[nrow][ncol]==1){
-                   q.offer(new Pair(nrow,ncol,curr_level+1));
-                   vis[nrow][ncol]=1;
-                   fr--;
-               }
-               
-           }
-           
-       }
-       if(fr!=0) return -1;
-       return max_level;
+class Solution {
+    public int orangesRotting(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] a = grid;
+        Queue<int[]> q = new LinkedList<>();
+        int f = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (a[i][j] == 2) {
+                    q.add(new int[] {i, j});
+                }
+                if (a[i][j] == 1) {
+                    f++;
+                }
+            }
+        }
+        if (f == 0)
+            return 0;
+        if (q.isEmpty())
+            return -1;
+        
+        int min = -1;
+        int[][] d = {{1, 0},{-1, 0},{0, -1},{0, 1}};
+        while (!q.isEmpty()) {
+            int s = q.size();
+            while (s-- > 0) {
+                int[] pos = q.poll();
+                int x = pos[0];
+                int y = pos[1];
+                for (int[] d1 : d) {
+                    int i = x + d1[0];
+                    int j = y + d1[1];
+                    if (i >= 0 && i < m && j >= 0 && j < n && a[i][j] == 1) {
+                        a[i][j] = 2;
+                        f--;
+                        q.add(new int[] {i, j});
+                    }
+                }
+            }
+            min++;
+        }
+        
+        if (f== 0){
+            return min;
+        }
+        return -1;
     }
 }

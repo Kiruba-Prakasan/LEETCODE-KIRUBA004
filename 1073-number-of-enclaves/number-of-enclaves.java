c12442d30ae38f3d24725584fcaa3737d6521269
Kiruba-Prakasan <124655692+Kiruba-Prakasan@users.noreply.github.com>
2024-11-05 11:11:57 +0530

    Time: 9 ms (61.03%) | Memory: 54.1 MB (78.13%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.java
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -1,64 +1,61 @@
-class Pair{
-    int r;
-    int c;
-    Pair(int r,int c){
-        this.r=r;
-        this.c=c;
+class Pair {
+    int r, c;
+    Pair(int r, int c) {
+        this.r = r;
+        this.c = c;
     }
 }
+
 class Solution {
     public int numEnclaves(int[][] grid) {
         Queue<Pair> q = new LinkedList<>();
-        int m = grid.length;
-        int n = grid[0].length;
+        int m = grid.length, n = grid[0].length;
         boolean[][] vis = new boolean[m][n];
-        //rows
-        for(int i=0;i<n;i++){
-            if(grid[0][i]==1){
-                q.add(new Pair(0,i));
-                vis[0][i]=true;
+
+        for (int i = 0; i < n; i++) {
+            if (grid[0][i] == 1) {
+                q.add(new Pair(0, i));
+                vis[0][i] = true;
             }
-            if(grid[m-1][i]==1){
-                q.add(new Pair(m-1,i));
-                vis[m-1][i]=true;
-            }
-        }
-        //cols
-        for(int i=0;i<m;i++){
-            if(grid[i][0]==1 && !vis[i][0]){
-                q.add(new Pair(i,0));
-                vis[i][0]=true;
-            }
-            if(grid[i][n-1]==1 && !vis[i][n-1]){
-                q.add(new Pair(i,n-1));
-                vis[i][n-1]=true;
+            if (grid[m - 1][i] == 1) {
+                q.add(new Pair(m - 1, i));
+                vis[m - 1][i] = true;
             }
         }
-        //BFS
-        int[] drow = {-1,0,1,0};
-        int[] dcol = {0,1,0,-1};
-        while(!q.isEmpty()){
-            int row = q.peek().r;
-            int col = q.peek().c;
+
+        for (int i = 0; i < m; i++) {
+            if (grid[i][0] == 1 && !vis[i][0]) {
+                q.add(new Pair(i, 0));
+                vis[i][0] = true;
+            }
+            if (grid[i][n - 1] == 1 && !vis[i][n - 1]) {
+                q.add(new Pair(i, n - 1));
+                vis[i][n - 1] = true;
+            }
+        }
+
+        int[] drow = {-1, 0, 1, 0};
+        int[] dcol = {0, 1, 0, -1};
+
+        while (!q.isEmpty()) {
+            int row = q.peek().r, col = q.peek().c;
             q.poll();
-            for(int i=0;i<4;i++){
-                int nr = row+drow[i];
-                int nc = col+dcol[i];
-                if(nr>=0 && nr<m && nc>=0 && nc<n && grid[nr][nc]==1 && !vis[nr][nc]){
-                    vis[nr][nc]=true;
-                    q.add(new Pair(nr,nc));
+            for (int i = 0; i < 4; i++) {
+                int nr = row + drow[i], nc = col + dcol[i];
+                if (nr >= 0 && nr < m && nc >= 0 && nc < n && grid[nr][nc] == 1 && !vis[nr][nc]) {
+                    vis[nr][nc] = true;
+                    q.add(new Pair(nr, nc));
                 }
             }
         }
-        //To find Enclave
+
         int ans = 0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]==1&&!vis[i][j]){
-                    ans++;
-                }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1 && !vis[i][j]) ans++;
             }
         }
+
         return ans;
     }
 }

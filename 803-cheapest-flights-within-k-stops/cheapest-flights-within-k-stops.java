602f45f225c3ce882e6c53d79be8f59886344532
Kiruba-Prakasan <124655692+Kiruba-Prakasan@users.noreply.github.com>
2024-11-07 13:57:34 +0530

    Time: 1 ms (99.48%) | Memory: 44.4 MB (47.49%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -1,19 +1,33 @@
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        int[] cost = new int[n];
-        Arrays.fill(cost, Integer.MAX_VALUE);
-        cost[src] = 0;
-        for (int i = 0; i <= k; i++) {
-            int[] temp = Arrays.copyOf(cost, n);
-            for (int[] f : flights) {
-                int u = f[0], v = f[1], p = f[2];
-                if (cost[u] == Integer.MAX_VALUE) {
-                    continue;
-                }
-                temp[v] = Math.min(temp[v], cost[u] + p);
+        int[] dist = new int[n];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[src] = 0;
+
+        for (int i = 0; i < k + 1; i++) {
+            if (bellmanFord(flights, dist)) {
+                break;
             }
-            cost = temp;
         }
-        return cost[dst] == Integer.MAX_VALUE ? -1 : cost[dst];
+
+        return dist[dst] == Integer.MAX_VALUE ? -1 : dist[dst];
+    }
+
+    private boolean bellmanFord(int[][] flights, int[] dist) {
+        int[] prevDist = dist.clone();
+        boolean finished = true;
+
+        for (int[] flight : flights) {
+            int u = flight[0];
+            int v = flight[1];
+            int weight = flight[2];
+
+            if (prevDist[u] != Integer.MAX_VALUE && prevDist[u] + weight < dist[v]) {
+                dist[v] = prevDist[u] + weight;
+                finished = false;
+            }
+        }
+
+        return finished;
     }
 }

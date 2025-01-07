fbf1320f0573846a3130b309467e1a435f1fadd3
Kiruba-Prakasan <124655692+Kiruba-Prakasan@users.noreply.github.com>
2025-01-07 20:25:42 +0530

    Time: 0 ms (100.00%) | Memory: 30.5 MB (18.22%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.c
+++ 238-product-of-array-except-self/product-of-array-except-self.c
@@ -1,4 +1,5 @@
 int* productExceptSelf(int* nums, int n, int* returnSize) {
+    /*
     *returnSize = n;
     int * res = (int*)malloc(n*sizeof(int));
     int prod = 1;
@@ -21,6 +22,23 @@ int* productExceptSelf(int* nums, int n, int* returnSize) {
         else{
             res[i]=prod/nums[i];
         }
+    }
+    return res;
+    */
+    int l[n];
+    int r[n];
+    *returnSize=n;
+    int* res = (int*)malloc(n*sizeof(int));
+    l[0]=1;
+    for(int i=1;i<n;i++){
+        l[i]=l[i-1]*nums[i-1];
+    }
+    r[n-1]=1;
+    for(int i=n-2;i>=0;i--){
+        r[i]=r[i+1]*nums[i+1];
+    }
+    for(int i=0;i<n;i++){
+        res[i] = l[i]*r[i];
     }
     return res;
 }

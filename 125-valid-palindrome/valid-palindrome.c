bdc013d2ee29fdd04318be70896848038e51c24a
Kiruba-Prakasan <124655692+Kiruba-Prakasan@users.noreply.github.com>
2025-01-09 20:35:23 +0530

    Time: 160 ms (5.09%) | Memory: 8.4 MB (92.32%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.c
@@ -0,0 +1,27 @@
+bool isPalindrome(char* s) {
+    int k=0;
+    for(int i=0;i<strlen(s);i++){
+        if((s[i]>='A' && s[i]<='Z') || (s[i]>='a' && s[i]<='z') || (s[i]>='0' && s[i]<='9')){
+            s[k++]=s[i];
+        }
+    }
+    s[k]='\0';
+    printf("%s",s);
+    int i=0,j=strlen(s)-1;
+    while(i<j){
+        char c1 = s[i];
+        char c2 = s[j];
+        if(c1>='A' && c1<='Z'){
+            c1 = c1+32;
+        }
+        if(c2>='A' && c2<='Z'){
+            c2 = c2+32;
+        }
+        if(c1!=c2){
+            return false;
+        }
+        i++;
+        j--;
+    }
+    return true;
+}

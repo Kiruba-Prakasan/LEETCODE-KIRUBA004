74fb780326a88546f0d999f285c948d1425a546c
Kiruba-Prakasan <124655692+Kiruba-Prakasan@users.noreply.github.com>
2025-02-14 15:14:23 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (67.82%) - LeetSync

---
+++ 100-same-tree/same-tree.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p==null&&q==null){
+            return true;
+        }
+        if(p==null||q==null){
+            return false;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(p);
+        queue.add(q);
+        while(!queue.isEmpty()){
+            TreeNode node1 = queue.poll();
+            TreeNode node2 = queue.poll();
+
+            if(node1==null&&node2==null){
+                continue;
+            }
+            if(node1==null||node2==null||node1.val!=node2.val){
+                return false;
+            }
+            queue.add(node1.left);
+            queue.add(node2.left);
+            queue.add(node1.right);
+            queue.add(node2.right);
+        }
+        return true;
+    }
+}
